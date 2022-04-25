db57fdca9094176f0fe3d2acce1b1015d88d06fe
seah78 <githubseah.pro>
2022-04-25 16:57:02 +0200

    update settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+
+load_dotenv('.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
